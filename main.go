e9d1ca6006b965b19080ecc76657fb9fb0cc98d9
MaysHroub <hroubmays@gmail.com>
2025-07-27 14:18:31 +0300

    refactor: remove repl logic to another file

--- main.go
+++ main.go
@@ -1,31 +1,8 @@
 package main
 
-import (
-  "fmt"
-  "strings"
-  "bufio"
-  "os"
-)
 
 func main() {
-  scanner := bufio.NewScanner(os.Stdin)
-
-  for {
-    fmt.Print("Pokedex > ")
-    
-    if !scanner.Scan() {
-      break
-    }  
-  
-    input := strings.TrimSpace(scanner.Text())
-    words := cleanInput(input)
-    fmt.Println("Your command was: " + words[0])
-  }  
+  startRepl()
 }
 
-
-func cleanInput(text string) []string {
-  text = strings.ToLower(text)
-  return strings.Fields(text)  
-}
 
---
+++ repl.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "fmt"
+  "strings"
+  "bufio"
+  "os"
+)
+
+func startRepl() {
+  scanner := bufio.NewScanner(os.Stdin)
+
+  for {
+    fmt.Print("Pokedex > ")
+    
+    if !scanner.Scan() {
+      break
+    }  
+  
+    input := strings.TrimSpace(scanner.Text())
+    words := cleanInput(input)
+
+    if len(words) == 0 {
+      continue
+    }
+
+    fmt.Println("Your command was: " + words[0])
+  }  
+}
+
+
+func cleanInput(text string) []string {
+  text = strings.ToLower(text)
+  return strings.Fields(text)  
+}
+
