6ab36bb5ae66f37a6e53a44a97588f72fbd9cd09
MaysHroub <hroubmays@gmail.com>
2025-07-28 17:40:42 +0300

    chore: move repl_test to test package

--- repl.go
+++ repl.go
@@ -1,36 +1,34 @@
 package main
 
 import (
-  "fmt"
-  "strings"
-  "bufio"
-  "os"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func startRepl() {
-  scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 
-  for {
-    fmt.Print("Pokedex > ")
-    
-    if !scanner.Scan() {
-      break
-    }  
-  
-    input := strings.TrimSpace(scanner.Text())
-    words := cleanInput(input)
+	for {
+		fmt.Print("Pokedex > ")
 
-    if len(words) == 0 {
-      continue
-    }
+		if !scanner.Scan() {
+			break
+		}
 
-    fmt.Println("Your command was: " + words[0])
-  }  
+		input := strings.TrimSpace(scanner.Text())
+		words := cleanInput(input)
+
+		if len(words) == 0 {
+			continue
+		}
+
+		fmt.Println("Your command was: " + words[0])
+	}
 }
-
 
 func cleanInput(text string) []string {
-  text = strings.ToLower(text)
-  return strings.Fields(text)  
+	text = strings.ToLower(text)
+	return strings.Fields(text)
 }
-
