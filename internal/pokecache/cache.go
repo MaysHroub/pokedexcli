6211e8ae8dc847a97d09becb3903f3c427f556e4
MaysHroub <hroubmays@gmail.com>
2025-07-29 18:57:59 +0300

    feat: implement reaping functionality for cache

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -6,16 +6,18 @@ import (
 )
 
 type Cache struct {
-	cacheEntries      map[string]cacheEntry
-	cacheEntryTimeout time.Duration
-	mutx              sync.Mutex
+	cacheEntries map[string]cacheEntry
+	interval     time.Duration
+	mutx         sync.Mutex
 }
 
-func NewCache(cacheTimeout time.Duration) Cache {
-	return Cache{
-		cacheEntries:      make(map[string]cacheEntry),
-		cacheEntryTimeout: cacheTimeout,
+func NewCache(interval time.Duration) *Cache {
+	cache := Cache{
+		cacheEntries: make(map[string]cacheEntry),
+		interval:     interval,
 	}
+	go cache.reapLoop()
+	return &cache
 }
 
 type cacheEntry struct {
@@ -33,11 +35,27 @@ func (c *Cache) Add(key string, data []byte) {
 }
 
 func (c *Cache) Get(key string) (data []byte, found bool) {
+	c.mutx.Lock()
 	entry, found := c.cacheEntries[key]
+	c.mutx.Unlock()
 
 	if found {
 		return entry.data, true
 	}
 
 	return nil, false
+}
+
+func (c *Cache) reapLoop() {
+	ticker := time.NewTicker(c.interval)
+	for {
+		<-ticker.C
+		c.mutx.Lock()
+		for key, entry := range c.cacheEntries {
+			if time.Since(entry.createdAt) > c.interval {
+				delete(c.cacheEntries, key)
+			}
+		}
+		c.mutx.Unlock()
+	}
 }
