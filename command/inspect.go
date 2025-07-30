028fd9b7345e3d0dabc014fd67dd6789286d2fc9
MaysHroub <hroubmays@gmail.com>
2025-07-30 21:33:47 +0300

    feat: implement inspect command

---
+++ command/inspect.go
@@ -0,0 +1,31 @@
+package command
+
+import (
+	"fmt"
+	"github/MaysHroub/pokedexcli/configuration"
+)
+
+func Inspect(cfg *configuration.Config, pokemonName string) error {
+	pokemonInfo, exists := cfg.Pokedex[pokemonName]
+
+	if !exists {
+		fmt.Println("You have not caught that pokemon yet")
+		return nil
+	}
+
+	fmt.Printf("Name: %v\n", pokemonInfo.Name)
+	fmt.Printf("Height: %v\n", pokemonInfo.Height)
+	fmt.Printf("Weight: %v\n", pokemonInfo.Weight)
+
+	fmt.Println("Stats:")
+	for _, stat := range pokemonInfo.Stats {
+		fmt.Printf("	-%v: %v\n", stat.Stat.Name, stat.BaseStat)
+	}
+
+	fmt.Println("Types:")
+	for _, typ := range pokemonInfo.Types {
+		fmt.Printf("	-%v\n", typ.Type.Name)
+	}
+
+	return nil
+}
--- command/registry.go
+++ command/registry.go
@@ -10,35 +10,39 @@ type CliCommand struct {
 
 func GetCommands() map[string]CliCommand {
 	return map[string]CliCommand{
-		"exit": CliCommand{
+		"exit": {
 			Name:        "exit",
 			Description: "Exit the Pokedex",
 			Callback:    Exit,
 		},
-		"help": CliCommand{
+		"help": {
 			Name:        "help",
 			Description: "Displays a help message",
 			Callback:    Help,
 		},
-		"mapn": CliCommand{
+		"mapn": {
 			Name:        "mapn",
 			Description: "Displays next 20 location areas",
 			Callback:    MapNext,
 		},
-		"mapp": CliCommand{
+		"mapp": {
 			Name:        "mapp",
 			Description: "Displays previous 20 location areas",
 			Callback:    MapPrev,
 		},
-		"explore": CliCommand{
+		"explore": {
 			Name:        "explore <location-name | location-id>",
 			Description: "Displays all pokemon names in given location area",
 			Callback:    Explore,
 		},
-		"catch": CliCommand{
+		"catch": {
 			Name:        "catch <pokemon-name>",
-			Description: "Catches a pokemon given its name; the higher the base experience of the pokemon, the harder to catch",
+			Description: "Catches a pokemon; the higher the base experience of the pokemon, the harder to catch",
 			Callback:    Catch,
+		}, "inspect": {
+			Name:        "inspect <pokemon-name>",
+			Description: "Displays name, height, weight, stats, and type(s) of the pokemon",
+			Callback:    Inspect,
 		},
 	}
 }
